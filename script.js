c9c51b99c05826c28b68047678832cfb895add75
Wahid Osman <wahidosman3@gmail.com>
2020-12-18 10:51:05 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+const newYear = '1 Jan 2021';
+
+function countdown() {
+    const newYearDate = new Date(newYear);
+    const currentDate = new Date();
+
+    const totalSeconds = ( newYearDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds/ 3600/24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minutesEl.innerHTML = minutes;
+    secondsEl.innerHTML = seconds;
+
+}
+
+/*function formatTime(time) {
+    return time < 10 ? '0' : time;
+}*/
+
+countdown();
+
+setInterval(countdown, 1000);
